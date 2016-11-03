2d568602460a2cfcfe21ad228650a31828116c12
YouMe2 <yaalex.devlab@gmail.com>
2016-11-03 16:07:34 +0100

    Added the TestMain.

---
+++ ProgOOPracticalClasses2016/src/series1/TestMain.java
@@ -0,0 +1,15 @@
+package series1;
+
+import acm.graphics.GLabel;
+import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
+
+public class TestMain extends GraphicsProgram{
+	
+	@Override
+	public void run() {
+		
+		add(new GLabel("hello,  wolrd!", 20, 30));
+	}
+
+}
